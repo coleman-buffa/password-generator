d9f2cbb37fa1f8fb7f01c473196cb91ab4fa8b25
Coleman Buffa <coleman.buffa@gmail.com>
2020-12-17 16:32:08 -0800

    Create an array based on user input which is viewable in the browser console.

--- script.js
+++ script.js
@@ -3,15 +3,58 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  // passwordText.value = password;
 
 }
 
-// Add event listener to generate button
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passwordBot = {
+  upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  numberAry: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+  specialChar: ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\"", "]", "^", "_", "`", "{", "|", "}", "~", "]", ","],
+  selectorAry: [], //built from user prompt by arrayConstructor();
+  pwLength: 0, //set from user prompt  
+  
+  //series of prompts to user to select arrays and password length. assume they follow the rules for first implementation
+
+  setParameters: function() {
+    this.pwLength = prompt("How long would you like your password? Please choose a number 8-128.");
+    console.log("pwLength = " + this.pwLength);
+    if (confirm("Do you want uppercase characters in your password?")) {
+      this.arrayConstructor(this.upperCase);
+    }
+    if (confirm("Do you want lowercase characters in your password?")) {
+      this.arrayConstructor(this.lowerCase);
+    }
+    if (confirm("Do you want numbers in your password?")) {
+      this.arrayConstructor(this.numberAry);
+    }
+    if (confirm("Do you want special characters in your password?")) {
+      this.arrayConstructor(this.specialChar);
+    }
+
+  },
+
+  arrayConstructor: function(ary) {
+    this.selectorAry = this.selectorAry.concat(ary);
+    console.log(this.selectorAry);
+  },
+
+  pwConstructor: function() {
+    //builds the password by generating a random number and appending it onto password
+    //potentially use a while loop -> while pwlength < desired length add a letter    
+  }
+
+}
+
+//Testing purposes only. This call saves having to hook into existing HTML for first implementation.
+passwordBot.setParameters();
 
 // Pseudocoding and project planning
 // Build a password generator that takes user input into account:
@@ -44,23 +87,7 @@ generateBtn.addEventListener("click", writePassword);
 //   array constructor - builds array inside object given user input
 //   password constructor - randomly picks a letter from the constructed array and adds it to password
 
-var passwordBot = {
-  upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  numberAry = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
-  specialChar = ["!", "\", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "]", ","],
-  selectorAry = [], //built from user prompt by arrayConstructor();
-  pwLength = 0, //set from user prompt
 
-  arrayConstructor: function() {
-    //builds the array password characters will be chosen from
-  }
-
-  pwConstructor: function() {
-    //builds the password by generating a random number and appending it onto password
-  }
-
-}
 
   
   
